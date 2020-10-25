e810505f3cedbe9998d829a58127398dee001e5b
Victor Avdeev <avdeevvictor95@gmail.com>
2020-10-25 06:57:03 +0300

    feat: add weather function

--- momentum/script/script.js
+++ momentum/script/script.js
@@ -7,9 +7,18 @@ let
  changeBackgroundPrev = document.querySelector(".changeBackgroundPrev"),
  changeBackgroundNext = document.querySelector(".changeBackgroundNext"),
  FocusHeader = document.querySelector(".FocusHeader"),
- newCitate = document.querySelector(".newCitate")
+ newCitate = document.querySelector(".newCitate"),
+ City = document.querySelector('.City'),
  //isNeedChangeBg = true,
- prevHourWhenBgChanged = -1;
+ backgroundSrcPath = "https://github.com/irinainina/ready-projects/tree/momentum/momentum/assets/images/",
+ // day = "day/",
+ // evening = "evening/",
+ // morning = "morning/",
+ // night = "night/",
+ gloHourForBackground = new Date().getHours();;
+ function createMassiveOfBackgrounds (){
+  //Math.floor(Math.random() * (6));
+ }
 
  function getDayOfWeek (day){
   let dayOfWweek = "";
@@ -50,8 +59,22 @@ let
  }
  function setBackground(imgID){
   document.body.style.backgroundImage = `url(./assets/${imgID}.jpg)`;
+  //backgroundSrcPath = "https://github.com/irinainina/ready-projects/tree/momentum/momentum/assets/images/",
+  //document.body.style.backgroundImage = `url("${backgroundSrcPath}${getPartOfDay(imgID)}/0${imgID%6 +1}.jpg")`;
+
  }
- function getGreeting (hour){
+ function setBackgroundExt(e){
+  let curbackgrountImfID = Number(document.body.style.backgroundImage.replace('url("./assets/', "").replace(`.jpg")`, ""));
+  if( e.target.innerText === "<" ){
+   document.body.style.backgroundImage = curbackgrountImfID === 0 ? `url(./assets/${23}.jpg)` : `url(./assets/${curbackgrountImfID-1}.jpg)`;
+  }
+  else{
+   document.body.style.backgroundImage = curbackgrountImfID === 23 ? `url(./assets/${0}.jpg)` : `url(./assets/${curbackgrountImfID+1}.jpg)`;
+  }
+  //isNeedChangeBg = false;
+  gloHourForBackground = new Date().getHours();
+ }
+ function getPartOfDay (hour){
   let PartOfDay = "";
   if ( hour >= 0 && hour < 6){
    PartOfDay = "night";
@@ -67,7 +90,7 @@ let
  }
  return PartOfDay;
 }
-
+setBackground(gloHourForBackground);
  function setDateTime (){
  let
   curDateTime = new Date(),
@@ -80,12 +103,12 @@ let
 
   time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
   date.innerText = `${getDayOfWeek(Day)}, ${getMonth(Month)} ${DaOfMonth}`;
-  greeting.textContent = `Good ${getGreeting(hour)}, `;
+  greeting.textContent = `Good ${getPartOfDay(hour)}, `;
 
-  if (prevHourWhenBgChanged !== hour){
+  if (gloHourForBackground !== hour){
    //isNeedChangeBg = true;
-   prevHourWhenBgChanged = hour;
-   setBackground(hour);
+   gloHourForBackground = hour;
+
   }
   // if (isNeedChangeBg){
 
@@ -94,17 +117,7 @@ let
   setTimeout(setDateTime, 1000);
 }
 
-function setBackgroundExt(e){
- let curbackgrountImfID = Number(document.body.style.backgroundImage.replace('url("./assets/', "").replace(`.jpg")`, ""));
- if( e.target.innerText === "<" ){
-  document.body.style.backgroundImage = curbackgrountImfID === 0 ? `url(./assets/${23}.jpg)` : `url(./assets/${curbackgrountImfID-1}.jpg)`;
- }
- else{
-  document.body.style.backgroundImage = curbackgrountImfID === 23 ? `url(./assets/${0}.jpg)` : `url(./assets/${curbackgrountImfID+1}.jpg)`;
- }
- //isNeedChangeBg = false;
- prevHourWhenBgChanged = new Date().getHours();
-}
+
 function getName(){
  let strForNameField = "";
  if (localStorage.getItem("name") === null || localStorage.getItem("name") === ""){
@@ -127,6 +140,32 @@ function getfocus(){
   strForfocusField = localStorage.getItem("focus");
  }
  focus.textContent = strForfocusField;
+}
+async function getWeather(_City) {
+ console.log(_City);
+ const url = `https://api.openweathermap.org/data/2.5/weather?q=${_City}&lang=en&appid=214da44507268740ef255574b1dda117&units=metric`;
+ const res = await fetch(url);
+ const data = await res.json();
+
+ const weatherIcon = document.querySelector('.weather-icon');
+ const temperature = document.querySelector('.temperature');
+ const weatherDescription = document.querySelector('.weather-description');
+ weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+ temperature.textContent = `${data.main.temp}°C`;
+ weatherDescription.textContent = `${data.weather[0].description}, humidity ${data.main.humidity}%, speed of wind ${data.wind.speed} m/c`;
+
+}
+function getCity(){
+ let strForCityField = "";
+ console.log(123);
+ if (localStorage.getItem("City") === null || localStorage.getItem("City") === ""){
+  strForCityField = "[Enter City there]";
+ }
+ else {
+  strForCityField = localStorage.getItem("City");
+  getWeather(strForCityField);
+ }
+ City.textContent = strForCityField;
 }
 function checkSTR(s){
  if ( s.length  === 0 || /^\s+$/.test(s) ){//пустая строка или строка из символов пустого пространства
@@ -171,7 +210,24 @@ function setfocus(e){
   }
  }
 }
-function getRandCitate(){
+function setCity(e){
+ if (e.type = "keypress" ){
+  if ( e.which == 13 || e.keyCode == 13 ){
+   if (checkSTR(e.target.innerText)){
+    localStorage.setItem("City",` ${e.target.innerText}`);
+   }
+   getCity();
+   City.blur();
+  }
+  else if (e.which == 1 || e.keyCode == 1) {
+   e.target.innerText = " ";
+  }
+  else if (e.which == 0 || e.keyCode == 0){
+   getCity();
+  }
+ }
+}
+async function getRandCitate(){
  let requestURL = "https://type.fit/api/quotes";
  let citates = {};
  let request = new XMLHttpRequest();
@@ -196,12 +252,22 @@ focus.addEventListener("keypress", function(e) {setfocus(e)});
 focus.addEventListener("click", function(e) {setfocus(e)});
 focus.addEventListener("blur", function(e) {setfocus(e)});
 
+City.addEventListener("keypress", function(e) {setCity(e)});
+City.addEventListener("click", function(e) {setCity(e)});
+City.addEventListener("blur", function(e) {setCity(e)});
+
 newCitate.addEventListener("click", function(e) {getRandCitate()});
 
 getName();
 getfocus();
 setDateTime();
 getRandCitate();
+getCity();
+
+
+
+
+
 
 
 
