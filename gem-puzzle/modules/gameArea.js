b5ff305f45f7db2f98a1074907063d3486e0f3fd
Victor Avdeev <avdeevvictor95@gmail.com>
2020-11-16 22:39:10 +0300

    feat: много всего

--- gem-puzzle/modules/gameArea.js
+++ gem-puzzle/modules/gameArea.js
@@ -1,5 +1,9 @@
 export default class GameArea {
   constructor() {
+    this.audioVictory = new Audio('./Sound/Victory.mp3');
+    this.audioYes = new Audio('./Sound/yes.mp3');
+    this.audioNo = new Audio('./Sound/no.mp3');
+
     this.gameArea = document.createElement('div');
     this.gameArea.classList.add('gameArea');
     document.body.appendChild(this.gameArea);
@@ -38,6 +42,11 @@ export default class GameArea {
     this.context = this.puzzleArea.getContext('2d');
     this.puzzleArea.width = 700;
     this.puzzleArea.height = 700;
+
+    this.gameArea.isAutoplay = false;
+    this.gameArea.Autoplay = null;
+
+    this.gameArea.timer = null;
   }
 
   getMainNode() {
@@ -52,10 +61,33 @@ export default class GameArea {
     return { mode: this.mode, score: this.score, time: this.time };
   }
 
-  testDrawExt(NumOfPieces = 8) {
+  updateTime() {
+    return setInterval(() => {
+      this.PuzzleObj.time += (!this.PuzzleObj.isResolved) ? 1 : 0;
+
+      this.time.textContent = `Time: ${this.addZero(Math.floor(this.PuzzleObj.time / 60))}:${this.addZero(this.PuzzleObj.time % 59 || 59)}`;
+    }, 1000);
+  }
+
+  addZero(strToParse) {
+    return ((parseInt(strToParse, 10) < 10) ? '0' : '') + strToParse;
+  }
+
+  DrawNewPuzzle(NumOfPieces = 3, image = '../images/1.jpg') {
+    this.clearCanvas();
+    this.clearGameInfo();
+    this.mode.textContent = `Mode: ${NumOfPieces}x${NumOfPieces}`;
     this.PuzzleObj = {};
     this.PuzzleObj.NumOfPieces = NumOfPieces;
-    this.PuzzleObj.image = '../images/1.jpg';
+    this.PuzzleObj.image = image;
+    this.PuzzleObj.Score = 0;
+    this.score.textContent = `Score: ${this.PuzzleObj.Score}`;
+    this.PuzzleObj.time = 0;
+    this.time.textContent = `Time: 00:${this.addZero(this.PuzzleObj.time)}`;
+    this.PuzzleObj.CanMove = true;
+    this.PuzzleObj.isResolved = false;
+    this.PuzzleObj.isUseSound = true;
+    this.PuzzleObj.isAutoplay = false;
     this.image = new Image();
     this.image.onload = () => {
       this.context = this.puzzleArea.getContext('2d');
@@ -65,16 +97,13 @@ export default class GameArea {
       const NumofPiecesOnCanvas = NumOfPieces ** 2;
       const usedPos = [];
       let numIter = 0;
-      // for (let i = 0; i < NumofPiecesOnCanvas; i++) {
       while (numIter < NumofPiecesOnCanvas) {
         const i = Math.floor(Math.random() * (NumofPiecesOnCanvas));
-
         if (usedPos.indexOf(i) === -1) {
-          // console.log(`${numIter} ${i}`);
-
           usedPos.push(i);
           this.PuzzleObj.Pieces[i] = {
             PosSrc: numIter,
+            PosDst: i,
             sx: (this.image.width / this.PuzzleObj.NumOfPieces) * (numIter % this.PuzzleObj.NumOfPieces),
             sy: (this.image.height / this.PuzzleObj.NumOfPieces) * Math.floor((numIter) / this.PuzzleObj.NumOfPieces),
             sWidth: this.image.width / NumOfPieces,
@@ -85,33 +114,10 @@ export default class GameArea {
             dHeight: this.PuzzleObj.itemSize - this.PuzzleObj.borderWeight * 2,
             isFree: (numIter === (NumofPiecesOnCanvas - 1)),
           };
+          this.PuzzleObj.Pieces[i].dxCenter = this.PuzzleObj.Pieces[i].dx + this.PuzzleObj.Pieces[i].dWidth / 2;
+          this.PuzzleObj.Pieces[i].dyCenter = this.PuzzleObj.Pieces[i].dy + this.PuzzleObj.Pieces[i].dHeight / 2;
+          this.drawPiece(this.PuzzleObj.Pieces[i]);
 
-          if (!this.PuzzleObj.Pieces[i].isFree) {
-            this.context.drawImage(this.image,
-              this.PuzzleObj.Pieces[i].sx,
-              this.PuzzleObj.Pieces[i].sy,
-              this.PuzzleObj.Pieces[i].sWidth,
-              this.PuzzleObj.Pieces[i].sHeight,
-              this.PuzzleObj.Pieces[i].dx,
-              this.PuzzleObj.Pieces[i].dy,
-              this.PuzzleObj.Pieces[i].dWidth,
-              this.PuzzleObj.Pieces[i].dHeight);
-
-            this.context.font = '20px Arial';
-            this.context.fillText(numIter + 1,
-              this.PuzzleObj.Pieces[i].dx + (this.PuzzleObj.Pieces[i].dWidth) / 2,
-              this.PuzzleObj.Pieces[i].dy + this.PuzzleObj.Pieces[i].dHeight / 2);
-          } else {
-            this.context.fillStyle = 'white';
-            this.context.fillRect(
-              this.PuzzleObj.Pieces[i].dx,
-              this.PuzzleObj.Pieces[i].dy,
-              this.PuzzleObj.Pieces[i].dWidth,
-              this.PuzzleObj.Pieces[i].dHeight,
-            );
-          }
-
-          // console.log(this.PuzzleObj);
           numIter += 1;
         }
       }
@@ -120,14 +126,202 @@ export default class GameArea {
       // console.log(JSON.parse(JSON.stringify(this.PuzzleObj)));
     };
     this.image.src = this.PuzzleObj.image;
+    if (this.gameArea.timer !== null) {
+      clearInterval(this.gameArea.timer);
+    }
+    this.gameArea.timer = this.updateTime();
   }
 
-  clear() {
-    console.log('111');
+  clearCanvas() {
     this.context.clearRect(0, 0, 700, 700);
+    return true;
   }
 
-  update(e) {
+  clearGameInfo() {
+    this.mode.textContent = 'Mode: ';
+    this.score.textContent = 'Score: ';
+    this.time.textContent = 'Time: ';
+    return true;
+  }
 
+  getPuzzlePieceByCoord(x, y) {
+    let indx;
+    this.PuzzleObj.Pieces.forEach((element, index) => {
+      if (x > element.dx
+           && x < (element.dx + element.dWidth)
+           && y > element.dy
+           && y < (element.dy + element.dHeight)) {
+        // obj = element;
+        indx = index;
+      }
+    });
+    return indx;
+  }
+
+  isMovablePiece(index) {
+    let isMovable = false;
+    const freeRect = this.PuzzleObj.Pieces[this.getFreePieseIndex()];
+    const piece = this.PuzzleObj.Pieces[index];
+    if (!piece.isFree) {
+      const spaceBtweenCenters = Math.sqrt(((freeRect.dxCenter - piece.dxCenter) ** 2) + ((freeRect.dyCenter - piece.dyCenter) ** 2));
+      isMovable = (spaceBtweenCenters <= freeRect.sWidth);
+    }
+    if (isMovable) {
+      this.doSound('Yes');
+    } else {
+      this.doSound('No');
+    }
+    return isMovable;
+  }
+
+  setMoveOption(opt) {
+    this.PuzzleObj.CanMove = opt;
+  }
+
+  move(index) {
+    const freeRect = this.getFreePieseIndex();
+    const buffS = {
+      PosDst: this.PuzzleObj.Pieces[index].PosDst,
+      dx: this.PuzzleObj.Pieces[index].dx,
+      dy: this.PuzzleObj.Pieces[index].dy,
+      dxCenter: this.PuzzleObj.Pieces[index].dxCenter,
+      dyCenter: this.PuzzleObj.Pieces[index].dyCenter,
+    };
+
+    this.PuzzleObj.Pieces[index].PosDst = this.PuzzleObj.Pieces[freeRect].PosDst;
+    this.PuzzleObj.Pieces[index].dx = this.PuzzleObj.Pieces[freeRect].dx;
+    this.PuzzleObj.Pieces[index].dy = this.PuzzleObj.Pieces[freeRect].dy;
+    this.PuzzleObj.Pieces[index].dxCenter = this.PuzzleObj.Pieces[freeRect].dxCenter;
+    this.PuzzleObj.Pieces[index].dyCenter = this.PuzzleObj.Pieces[freeRect].dyCenter;
+    this.drawPiece(this.PuzzleObj.Pieces[index]);
+
+    this.PuzzleObj.Pieces[freeRect].PosDst = buffS.PosDst;
+    this.PuzzleObj.Pieces[freeRect].dx = buffS.dx;
+    this.PuzzleObj.Pieces[freeRect].dy = buffS.dy;
+    this.PuzzleObj.Pieces[freeRect].dxCenter = buffS.dxCenter;
+    this.PuzzleObj.Pieces[freeRect].dyCenter = buffS.dyCenter;
+    this.drawPiece(this.PuzzleObj.Pieces[freeRect]);
+
+    this.PuzzleObj.Score += 1;
+    this.score.textContent = `Score: ${this.PuzzleObj.Score}`;
+
+    if (this.CheckIfPuzzleComplete()) {
+      this.doSound('Victory');
+      this.PuzzleObj.isResolved = true;
+      this.PuzzleObj.CanMove = false;
+      const message = `Ура! Вы решили головоломку за ${this.addZero(Math.floor(this.PuzzleObj.time / 60))}:${this.addZero(this.PuzzleObj.time % 59 || 59)} и ${this.PuzzleObj.Score} ходов`;
+      setTimeout(() => {
+        alert(message);
+      }, 1000);
+    }
+  }
+
+  getFreePieseIndex() {
+    let forret;
+    this.PuzzleObj.Pieces.forEach((element, index) => {
+      if (element.isFree) {
+        forret = index;
+      }
+    });
+    return forret;
+  }
+
+  getPieseIndexbyPosSrc(PosSrc) {
+    let forret;
+    this.PuzzleObj.Pieces.forEach((element, index) => {
+      if (element.PosSrc === PosSrc) {
+        forret = index;
+      }
+    });
+    return forret;
+  }
+
+  isPieceResolvedByPosSrc(PosSrc) {
+    let forret = false;
+    const element = this.PuzzleObj.Pieces[this.getPieseIndexbyPosSrc(PosSrc)];
+    if (element.PosSrc === element.PosDst) {
+      forret = true;
+    }
+    return forret;
+  }
+
+  drawPiece(piece) {
+    if (!piece.isFree) {
+      this.context.drawImage(this.image,
+        piece.sx,
+        piece.sy,
+        piece.sWidth,
+        piece.sHeight,
+        piece.dx,
+        piece.dy,
+        piece.dWidth,
+        piece.dHeight);
+
+      this.context.font = '26px Arial';
+      this.context.fillStyle = 'white';
+      this.context.fillText(piece.PosSrc,
+        piece.dxCenter,
+        piece.dyCenter);
+    } else {
+      this.context.fillStyle = 'white';
+      this.context.fillRect(
+        piece.dx,
+        piece.dy,
+        piece.dWidth,
+        piece.dHeight,
+      );
+    }
+  }
+
+  CheckIfPuzzleComplete() {
+    let isComplete = true;
+    this.PuzzleObj.Pieces.forEach((element) => {
+      if (element.PosSrc !== element.PosDst) {
+        isComplete = false;
+      }
+    });
+    return isComplete;
+  }
+
+  doSound(type) {
+    if (this.PuzzleObj.isUseSound) {
+      switch (type) {
+        case 'Yes':
+          this.audioYes.play(); break;
+        case 'No':
+          this.audioNo.play(); break;
+        case 'Victory':
+          this.audioVictory.play(); break;
+        default: break;
+      }
+    }
+  }
+
+  getMinStartPos() {
+    let minVal = 0;
+    let i = 0;
+    while (this.isPieceResolvedByPosSrc(i)) {
+      minVal += 1;
+      i += 1;
+    }
+    return minVal;
+  }
+
+  tryToResolve() {
+    if (this.gameArea.isAutoplay) {
+      return setInterval(() => {
+        this.PuzzleObj.isUseSound = false;
+        if (!this.PuzzleObj.isResolved) {
+          const min = this.getMinStartPos();
+          const max = (this.PuzzleObj.NumOfPieces ** 2);
+          const i = Math.floor(Math.random() * (max - min)) + min;
+          const j = this.getPieseIndexbyPosSrc(i);
+          if (this.isMovablePiece(j)) {
+            this.move(j);
+          }
+        }
+        this.PuzzleObj.isUseSound = true;
+      }, 1);
+    }
   }
 }
--- gem-puzzle/modules/menu.js
+++ gem-puzzle/modules/menu.js
@@ -1,47 +1,130 @@
 export default class Menu {
   constructor(parentNode) {
     this.parentNode = parentNode;
-    this.menuItems = ['Continue', 'New', 'Load', 'Save', 'Score'];
-    this.isVisible = true;
+    // main menu
+    this.MainMenu = {};
+    this.MainMenu.DOM = document.createElement('div');
+    this.MainMenu.DOM.classList.add('gameArea-menu');
+    this.parentNode.appendChild(this.MainMenu.DOM);
 
-    this.menu = document.createElement('div');
-    this.menu.classList.add('gameArea-menu');
-    this.parentNode.appendChild(this.menu);
+    this.MainMenu.Items = ['New', 'Autoplay', 'Settings'];
+    this.MainMenu.isVisible = true;
+    this.MainMenu.menuList = {};
+    this.MainMenu.menuList.DOM = document.createElement('li');
+    this.MainMenu.DOM.appendChild(this.MainMenu.menuList.DOM);
+    this.MainMenu.menuList.ListItem = {};
 
-    this.menuList = document.createElement('li');
-    this.menu.appendChild(this.menuList);
-    this.menuListItem = {};
-    this.menuItems.forEach((element) => {
-      this.menuListItem[element] = {
+    this.MainMenu.Items.forEach((element) => {
+      this.MainMenu.menuList.ListItem[element] = {
         name: element,
         isActive: false,
-        Dom_Element: document.createElement('ul'),
+        DOM: document.createElement('ul'),
       };
-      this.menuListItem[element].Dom_Element.textContent = element;
-      this.menuList.appendChild(this.menuListItem[element].Dom_Element);
+      this.MainMenu.menuList.ListItem[element].DOM.textContent = element;
+      this.MainMenu.menuList.DOM.appendChild(this.MainMenu.menuList.ListItem[element].DOM);
+      this.MainMenu.menuList.ListItem[element].DOM.classList.add('gameArea-menu-item');
     });
+    this.setMenuVisible(this.MainMenu);
+    // NG menu
+    this.Settings = {};
+    this.Settings.DOM = document.createElement('div');
+    this.Settings.DOM.classList.add('gameArea-menu');
+    this.parentNode.appendChild(this.Settings.DOM);
+    this.Settings.isVisible = true;
+
+    this.Settings.LI = document.createElement('li');
+    this.Settings.DOM.appendChild(this.Settings.LI);
+    // NG mode
+    this.Settings.UL_MODE = document.createElement('ul');
+    this.Settings.UL_MODE.textContent = 'Mode:';
+    this.Settings.LI.appendChild(this.Settings.UL_MODE);
+    this.Settings.UL_MODE.classList.add('gameArea-menu-item');
+
+    this.Settings.UL_MODE_SEL = document.createElement('select');
+    this.Settings.UL_MODE.appendChild(this.Settings.UL_MODE_SEL);
+    for (let i = 2; i <= 8; i++) {
+      const opt = `${i}x${i}`;
+      this.Settings[`UL_MODE_SEL_OPT_${i}`] = document.createElement('option');
+      this.Settings[`UL_MODE_SEL_OPT_${i}`].textContent = opt;
+      this.Settings[`UL_MODE_SEL_OPT_${i}`].value = i;
+      this.Settings.UL_MODE_SEL.appendChild(this.Settings[`UL_MODE_SEL_OPT_${i}`]);
+    }
+    // NG IMG
+    this.Settings.UL_IMG = document.createElement('ul');
+    this.Settings.UL_IMG.textContent = 'Chose image:';
+    this.Settings.UL_IMG.classList.add('gameArea-menu-item');
+    this.Settings.LI.appendChild(this.Settings.UL_IMG);
+    this.Settings.UL_IMG.appendChild(document.createElement('br'));
+    this.Settings.UL_IMG_IMAGE = document.createElement('img');
+    this.Settings.UL_IMG_IMAGE.src = '../images/1.jpg';
+    this.Settings.UL_IMG_IMAGE.id = 'settings_img';
+    this.Settings.UL_IMG_IMAGE.width = 600;
+    this.Settings.UL_IMG_IMAGE.height = 600;
+    this.Settings.UL_IMG.appendChild(this.Settings.UL_IMG_IMAGE);
+    this.Settings.UL_IMG.appendChild(document.createElement('br'));
+
+    this.Settings.UL_IMG_prev = document.createElement('span');
+    this.Settings.UL_IMG_prev.textContent = '<<prev ';
+    this.Settings.UL_IMG.appendChild(this.Settings.UL_IMG_prev);
+    this.Settings.UL_IMG_prev.classList.add('gameArea-menu-item');
+    this.Settings.UL_IMG_prev.addEventListener('click', () => {
+      const path = this.Settings.UL_IMG_IMAGE.src;
+      const num = Number(path.substring(path.length - 5, path.length - 4)) - 1;
+      this.Settings.UL_IMG_IMAGE.src = `../images/${num % 9 || 9}.jpg`;
+    });
+
+    this.Settings.UL_IMG_Next = document.createElement('span');
+    this.Settings.UL_IMG_Next.textContent = ' next>>';
+    this.Settings.UL_IMG.appendChild(this.Settings.UL_IMG_Next);
+    this.Settings.UL_IMG_Next.classList.add('gameArea-menu-item');
+    this.Settings.UL_IMG_Next.addEventListener('click', () => {
+      const path = this.Settings.UL_IMG_IMAGE.src;
+      const num = Number(path.substring(path.length - 5, path.length - 4)) + 1;
+      this.Settings.UL_IMG_IMAGE.src = `../images/${num % 9 || 9}.jpg`;
+    });
+    // NG back
+    this.Settings.UL_BACK = document.createElement('ul');
+    this.Settings.UL_BACK.textContent = 'Back';
+    this.Settings.UL_BACK.addEventListener('click', () => {
+      this.setMenuVisible(this.Settings);
+      this.setMenuVisible(this.MainMenu);
+    });
+    this.Settings.LI.appendChild(this.Settings.UL_BACK);
+    this.Settings.UL_BACK.classList.add('gameArea-menu-item');
+    this.setMenuVisible(this.Settings);
   }
 
   menuHandler(e) {
-    console.log(e.target.innerText);
-    switch (e.target.innerText) {
+    const type = e.target.innerText;
+    switch (type) {
       case 'Menu':
-        this.setMenuVisible();
+        this.setMenuVisible(this.MainMenu);
         break;
-      case 'New': break;
+      case 'New':
+        this.StartNewGame(e.target.innerText);
+        break;
+      case 'Settings':
+        this.setMenuVisible(this.MainMenu);
+        this.setMenuVisible(this.Settings);
+
+        break;
       default: break;
     }
   }
 
-  setMenuVisible() {
-    if (this.isVisible) {
-      this.menu.classList.add('gameArea-menu_invisible');
-      this.menu.classList.remove('gameArea-menu_visible');
-      this.isVisible = false;
+  getSettings() {
+    return this.Settings;
+  }
+
+  setMenuVisible(menuList) {
+    if (menuList.isVisible) {
+      menuList.DOM.classList.add('gameArea-menu_invisible');
+      menuList.DOM.classList.remove('gameArea-menu_visible');
+      menuList.isVisible = false;
     } else {
-      this.menu.classList.add('gameArea-menu_visible');
-      this.menu.classList.remove('gameArea-menu_invisible');
-      this.isVisible = true;
+      menuList.DOM.classList.add('gameArea-menu_visible');
+      menuList.DOM.classList.remove('gameArea-menu_invisible');
+      menuList.isVisible = true;
     }
   }
 }
