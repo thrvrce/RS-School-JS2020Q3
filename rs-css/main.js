065f3ed86eb73cd62cc0718f42dae32a746a400a
Victor Avdeev <avdeevvictor95@gmail.com>
2020-12-07 20:18:39 +0300

    feat: add 1-3 levels, check css selector function

--- rs-css/main.js
+++ rs-css/main.js
@@ -1,3 +1,8 @@
+import TypeLevel from './modules/TypeLevel.js';
+import classLevel from './modules/ClassLevel.js';
+import IDLevel from './modules/IDLevel.js';
+
+console.log(classLevel);
 const testLevel = {
   items: [
     {
@@ -8,6 +13,7 @@ const testLevel = {
       childrens: [
         {
           item: 'fish',
+          id: 'frst_fish',
           isSelectable: true,
           parent: null,
           childrens: undefined,
@@ -55,6 +61,7 @@ const testLevel = {
     },
     {
       item: 'octopus',
+      class: 'octopusWithGoogles',
       isSelectable: true,
       parent: null,
       childrensFlexProp: 'row',
@@ -80,38 +87,82 @@ const testLevel = {
 function createLevel(Items) {
   const levelFragment = {
     aquariumFragment: document.createDocumentFragment(),
-    FragmentHtml: '',
+    FragmentHtml: document.createDocumentFragment(),
   };
 
-  // const aquariumFragment = document.createDocumentFragment();
-  // let FragmentHtml = '';
   Items.forEach((element) => {
     element.aquariumElement = document.createElement(element.item);
-    // FragmentHtml += `\n<div> < <span> ${element.item} id = "" class = "" </span> > </div>`;
-    levelFragment.FragmentHtml += `\n<div><<span>${element.item} id = "" class = ""</span>>`;
+    element.FragmentHtmlElement = document.createElement('div');
+
+    element.FragmentHtmlElement.innerHTML += `&lt;${element.item}`;
+
+    if (element.id) {
+      element.aquariumElement.id = element.id;
+      element.FragmentHtmlElement.innerHTML += ` id = "${element.id}"`;
+    }
+
+    if (element.class) {
+      element.aquariumElement.classList.add(element.class);
+      element.FragmentHtmlElement.innerHTML += ` class = "${element.class}"`;
+    }
+
+    element.FragmentHtmlElement.innerHTML += `${(!element.childrens ? '\\' : '')}&gt;`;
+
     if (element.isSelectable) {
       element.aquariumElement.classList.add('selectable');
     }
     if (element.childrens) {
-      // element.aquariumElement.appendChild(createLevel(element.childrens));
       const childLevelFragment = createLevel(element.childrens);
       element.aquariumElement.appendChild(childLevelFragment.aquariumFragment);
       element.aquariumElement.classList.add(element.childrensFlexProp);
-      levelFragment.FragmentHtml += `\n  ${childLevelFragment.FragmentHtml}\n<div><<span>${element.item}\\</span>>`;
+
+      element.FragmentHtmlElement.appendChild(childLevelFragment.FragmentHtml);
+      element.FragmentHtmlElement.innerHTML += `&lt;${element.item}\\&gt;`;
     }
     levelFragment.aquariumFragment.appendChild(element.aquariumElement);
-    levelFragment.FragmentHtml += '</div>';
+
+    levelFragment.FragmentHtml.appendChild(element.FragmentHtmlElement);
   });
-  console.log(levelFragment.FragmentHtml);
-  // return aquariumFragment;
+
   return levelFragment;
+}
+
+function checkCssSelector(selectorToTest, selectorFromCurLevel) {
+  const NodesForTest = document.querySelectorAll(selectorToTest);
+  const NodesFromLevel = document.querySelectorAll(selectorFromCurLevel);
+  let isEqualNodes = false;
+  if (NodesForTest.length === NodesFromLevel.length) {
+    isEqualNodes = true;
+    for (let i = 0; i < NodesFromLevel.length; i++) {
+      // console.log(NodesForTest[i]);
+      // console.log(NodesFromLevel[i]);
+      // console.log(NodesForTest[i] === NodesFromLevel[i]);
+      if (NodesForTest[i] !== NodesFromLevel[i]) {
+        isEqualNodes = false;
+        break;
+      }
+    }
+  }
+  console.log(isEqualNodes);
+  return isEqualNodes;
 }
 
 const aquariumNode = document.querySelector('.aquarium');
 const HTMLcodeNode = document.querySelector('.HTMLCodeNode');
-// console.log(aquariumNode);
-const level = createLevel(testLevel.items);
+const curentLevel = IDLevel;
+const level = createLevel(curentLevel.items);
 aquariumNode.appendChild(level.aquariumFragment);
-HTMLcodeNode.innerHTML = level.FragmentHtml;
 
-// console.log(level.aquariumFragment);
+HTMLcodeNode.appendChild(level.FragmentHtml);
+
+const enterButton = document.querySelector('.enter');
+const inputCssSelector = document.querySelector('.selectorInput');
+enterButton.addEventListener('click', (e) => {
+  if (inputCssSelector.value) {
+    if (checkCssSelector(inputCssSelector.value, curentLevel.selector)) {
+
+    }
+  }
+
+  // console.log(document.querySelectorAll(inputCssSelector.value));
+});
